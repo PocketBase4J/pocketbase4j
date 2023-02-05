3263bfe905ef5de7d2c67896d341df8734c151dd
Andreas Sünder <asuender@student.tgm.ac.at>
2023-02-05 19:22:42 +0100

    Shorten authPrefix() method

--- src/main/java/lol/hub/pocketbase/AuthRole.java
+++ src/main/java/lol/hub/pocketbase/AuthRole.java
@@ -8,6 +8,6 @@ public enum AuthRole {
     }
 
     public String authPrefix() {
-        return this.name().substring(0, 1).toUpperCase() + this.name().substring(1).toLowerCase() + " ";
+        return this.toString();
     }
 }
