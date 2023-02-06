53e6cd499f173ec0e1459308115077a65c5cd544
Andreas Sünder <asuender@student.tgm.ac.at>
2023-02-06 21:53:52 +0100

    Implement first methods in RecordService

--- src/main/java/lol/hub/pocketbase/services/RecordService.java
+++ src/main/java/lol/hub/pocketbase/services/RecordService.java
@@ -1,7 +1,11 @@
 package lol.hub.pocketbase.services;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lol.hub.pocketbase.ApiClient;
+import lol.hub.pocketbase.models.ApiError;
+import lol.hub.pocketbase.models.Page;
+import lol.hub.pocketbase.models.Record;
 
 /**
  * <a href="https://pocketbase.io/docs/api-records/">api docs</a>
@@ -11,11 +15,15 @@ public class RecordService extends BaseService {
         super(http, gson);
     }
 
-    /* TODO:
-	GET    "/api/collections/:collection/records"
-	POST   "/api/collections/:collection/records"
-	GET    "/api/collections/:collection/records/:id"
-	PATCH  "/api/collections/:collection/records/:id"
-	DELETE "/api/collections/:collection/records/:id"
-     */
+    // TODO: add missing parameters
+    public Page<Record> listRecords(String collectionIdOrName) throws ApiError {
+        return http.send("GET", "/api/collections/" + collectionIdOrName + "/records", "", TypeToken.getParameterized(Page.class, Record.class).getType());
+    }
+
+    // TODO: add missing parameters
+    public Record viewRecord(String collectionIdOrName, String recordId) throws ApiError {
+        return http.send("GET", "/api/collections/" + collectionIdOrName + "/records/" + recordId, "", Record.class);
+    }
+
+    // TODO: implement missing methods
 }
