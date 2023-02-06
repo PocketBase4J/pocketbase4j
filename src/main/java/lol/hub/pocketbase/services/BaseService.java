ab44ad2d71f11384b7c1b0fed973e49b3e0c012a
Andreas Sünder <asuender@student.tgm.ac.at>
2023-02-06 21:44:10 +0100

    Protect fields in BaseService

--- src/main/java/lol/hub/pocketbase/services/BaseService.java
+++ src/main/java/lol/hub/pocketbase/services/BaseService.java
@@ -6,9 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class BaseService {
-    final Logger log;
-    final ApiClient http;
-    final Gson gson;
+    protected final Logger log;
+    protected final ApiClient http;
+    protected Gson gson;
 
     public BaseService(ApiClient http, Gson gson) {
         this.log = LoggerFactory.getLogger(this.getClass());
